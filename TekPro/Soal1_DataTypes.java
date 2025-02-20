5547a2506a38379c19999745c6c77a3d4b8d5147
ZaidanZS <voynobu@gmail.com>
2025-02-20 21:55:07 +0700

    Add files via upload

---
+++ TekPro/Soal1_DataTypes.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;//Digunakan agar dapat menerima input
+
+public class Soal1_DataTypes {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt();
+        int i = 0;
+
+        while (i<T){
+            try {
+                boolean valid = false;
+                long n = scn.nextLong();
+
+                System.out.println(n + " can be fitted in: ");
+                if (n <= Byte.MAX_VALUE && n >= Byte.MIN_VALUE) {
+                    System.out.println("* byte");
+                    valid = true;
+                }
+                if (n <= Short.MAX_VALUE && n >= Short.MIN_VALUE) {
+                    System.out.println("* short");
+                    valid = true;
+                }
+                if (n <= Integer.MAX_VALUE && n >= Integer.MIN_VALUE) {
+                    System.out.println("* int");
+                    valid = true;
+                }
+                if (n <= Long.MAX_VALUE && n >= Long.MIN_VALUE) {
+                    System.out.println("* long");
+                    valid = true;
+                }
+                if (!valid) {
+                    System.out.println(n + "can’t be fitted anywhere");
+                }
+            } catch (Exception e) {
+                System.out.println(scn.next() + " can't be fitted anywhere.");
+            }
+            i = i + 1;
+        }
+    }
+}
+
---
+++ TekPro/Soal5_String.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Soal5_String {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String A = scan.nextLine();
+        String B = scan.nextLine();
+        int a = A.length();
+        int b = B.length();
+        int Total_lenght = a + b;
+        System.out.println(Total_lenght);
+
+        int Hasil_perbandingan = A.compareTo(B);
+        if (Hasil_perbandingan < 0){
+            System.out.println("False");
+        } else {
+            System.out.println("True");
+        }
+
+        char[] arr1 = A.toCharArray();
+        char[] arr2 = B.toCharArray();
+        arr1[0] = Character.toUpperCase(arr1[0]);
+        arr2[0] = Character.toUpperCase(arr2[0]);
+        String str = new String(arr1);
+        String ing = new String(arr2);
+        System.out.println(str+" "+ing);
+    }
+}
